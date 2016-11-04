55675cac603e8a78dca28ca44bb56221794c4ceb
heyzqt <729224643@qq.com>
2016-11-04 13:19:42 +0800

    添加网络权限 请求一个JSON数据

--- app/src/main/java/com/zcc/zqtdemo/activity/MainActivity.java
+++ app/src/main/java/com/zcc/zqtdemo/activity/MainActivity.java
@@ -2,10 +2,25 @@ package com.zcc.zqtdemo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import com.zcc.zqtdemo.R;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,12 +34,39 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.edt_password)
     EditText mPassword;
 
-    @OnClick(R.id.button) void click(){
-        String name = mUsername.getText().toString().trim();
-        String password = mUsername.getText().toString().trim();
-        if(name.equals("123")&&password.equals("123")){
-            Toast.makeText(MainActivity.this, "log in", Toast.LENGTH_SHORT).show();
-        }
+    @BindView(R.id.tv_textarea)
+    TextView mTextArea;
+
+    private static final String TAG = "hello";
+
+
+    @OnClick(R.id.button)
+    void click() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest json = new JsonObjectRequest(Request.Method.POST, "http://route.showapi.com/9-7",
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(MainActivity.this, "连接网络", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "onResponse: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: " + "连接失败" + error.toString());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("showapi_appid", "22455");
+                map.put("areaid", "101010100");
+                map.put("month", "201611");
+                map.put("showapi_sign", "a23624424414443081dfd587547cf70b");
+                return map;
+            }
+        };
+        requestQueue.add(json);
     }
 
     @Override
